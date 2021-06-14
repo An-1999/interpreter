c6ce4dc782c00924fdec91fb432e01042adc46ae
An-1999 <79268348+An-1999@users.noreply.github.com>
2021-06-15 02:29:36 +0400

    Add files via upload

---
+++ example1.ml
@@ -0,0 +1,17 @@
+tivI n
+mutq <- n
+
+tivI s = 1
+ete(n >= 0) ->
+#    elq -> 1
+<-
+
+cikl (n != 0) ->
+    s = (s * n)
+    n = (n - 1)
+    ete (n == 1) ->
+        elq -> [fghjk]
+    <-
+<-
+elq -> s
+
