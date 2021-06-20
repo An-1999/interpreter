26af3fda33e966207afd80e4eeec4307df823a60
An-1999 <79268348+An-1999@users.noreply.github.com>
2021-06-20 15:45:29 +0400

    Update forif.ml

--- forif.ml
+++ forif.ml
@@ -7,7 +7,7 @@
 #        s = (s * n)
 #        n = (n - 1)
 #        ete (n == 1) ->
-#            elq -> [hjnu]
+#            elq -> [hi]
 #        <-
 #    elq -> 1
 #    <-
@@ -16,20 +16,36 @@
 #elq -> s
 
 
+#tivI w
+#mutq <- w
+#tivI s = 1
+#tivI i = 1
+#tivI n = i
+#cikl (i != (w + 1) ->
+#    n = i
+#    ete(n > 0) ->
+#         s = 1
+#        cikl (n > 0) ->
+#            s = (s * n)
+#            n = (n - 1)
+#        <-
+#    elq -> s
+#    <-
+#    i = (i + 1)
+# <-
+#
+
 tivI w
 mutq <- w
-tivI s = 1
 tivI i = 1
-tivI n = i
 cikl (i != (w + 1) ->
-    n = i
+    tivI n = i
     ete(n > 0) ->
-         s = 1
         cikl (n > 0) ->
-            s = (s * n)
+            elq -> n
             n = (n - 1)
         <-
-    elq -> s
+#elq -> s
     <-
     i = (i + 1)
  <-
